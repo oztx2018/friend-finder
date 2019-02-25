f5a9187d9578c17fb9887ec7741d14a4ffabe318
oztx2018 <41170781+oztx2018@users.noreply.github.com>
2019-02-24 20:40:57 -0600

    Update htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,6 @@ module.exports = function(app) {
     app.get("/survey", function(req,res){
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     }); 
-
     app.use(function(req,res){
         res.sendFile(path.join(__dirname + "/../public/home.html")); 
     });
